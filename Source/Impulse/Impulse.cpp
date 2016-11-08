29589be0165b953ce47a9aa34cd9fd705def06a0
Hugo Verger <hverger@wanadoo.fr>
2016-11-08 18:28:45 +0100

    fixed 1

--- Source/Impulse/Impulse.cpp
+++
@@ -1,5 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "Impulse.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Impulse, "Impulse" );
